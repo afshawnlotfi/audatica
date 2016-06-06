07ff72fc13ba4ed9633e1ffb23b80e0bdd7f11e0
Afshawn Lotfi <me@afshawn.org>
2016-06-06 11:14:04 -0400

    Add File Action Module

--- algorithm/backend/fileaction/Launch.py
+++ algorithm/backend/fileaction/Launch.py
@@ -1,11 +1,6 @@
 import os
 
 
-class Delete:
-    def __init__(self, path, filename):
-        os.system("rm -rf %s%s" % (path, filename))
-
-
 class Launch:
     def __init__(self, application, path, filename):
         os.system("%s %s%s" % (application, path, filename))
